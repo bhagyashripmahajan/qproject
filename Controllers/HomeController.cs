cb5e449a844e638c8463ea2131357e29ba0e03fd
bhagyashripmahajan <72036766+bhagyashripmahajan@users.noreply.github.com>
2022-01-30 18:40:50 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using qproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var employees = dc.Employees.OrderBy(a => a.Email).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Employees.Where(a => a.Id == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee emp)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    if (emp.Id > 0)
+                    {
+                        //Edit
+                        var v = dc.Employees.Where(a => a.Id == emp.Id).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.Email = emp.Email;
+                            v.Name = emp.Name;
+                            v.Image = emp.Image;
+                        }
+                    }
+                    else
+                    {
+                        //save
+                        dc.Employees.Add(emp);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Employees.Where(a => a.Id == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+    
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using (MyDatabaseEntities dc=new MyDatabaseEntities())
+            {
+                var v = dc.Employees.Where(a => a.Id == id).FirstOrDefault();
+                if(v!=null)
+                {
+                    dc.Employees.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new {status=status}  };
+        }
+    }
+}
---
+++ Models/txtended/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace qproject.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+    public class EmployeeMetadata
+    {
+        [Required (AllowEmptyStrings =false,ErrorMessage ="Please provide email")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide name")]
+        public string Name { get; set; }
+    }
+}
